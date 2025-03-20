500bb0a9bd6850f6fe42c255de1c221c09721754
SamahaRizvi <k214578@nu.edu.pk>
2025-03-20 16:36:46 +0500

    Enhance PostBloc with throttling for event handling and improve post fetching logic

--- flutter_infinite_list/lib/posts/bloc/post_bloc.dart
+++ flutter_infinite_list/lib/posts/bloc/post_bloc.dart
@@ -1,39 +1,80 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
-import 'package:flutter_infinite_list/bloc/bloc.dart';
-import 'package:flutter_infinite_list/post.dart';
+import 'package:bloc/bloc.dart';
+import 'package:bloc_concurrency/bloc_concurrency.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_infinite_list/posts/posts.dart';
+import 'package:http/http.dart' as http;
+import 'package:stream_transform/stream_transform.dart';
 
 part 'post_event.dart';
 part 'post_state.dart';
 
-class PostBloc extends Bloc<PostEvent, PostState> {
- PostBloc({required this.httpClient}) : super(const PostState()) {
-  on<PostFetched>(_onFetched);
+const _postLimit = 20;
+const throttleDuration = Duration(milliseconds: 100);
+
+EventTransformer<E> throttleDroppable<E>(Duration duration) {
+  return (events, mapper) {
+    return droppable<E>().call(events.throttle(duration), mapper);
+  };
 }
 
-Future<void> _onFetched(PostFetched event, Emitter<PostState> emit) async {
-  if (state.hasReachedMax) return;
+class PostBloc extends Bloc<PostEvent, PostState> {
+  PostBloc({required this.httpClient}) : super(const PostState()) {
+    on<PostFetched>(
+      _onFetched,
+      transformer: throttleDroppable(throttleDuration),
+    );
+  }
 
-  try {
-    final posts = await _fetchPosts(startIndex: state.posts.length);
+  final http.Client httpClient;
 
-    if (posts.isEmpty) {
-      return emit(state.copyWith(hasReachedMax: true));
+  Future<void> _onFetched(
+    PostFetched event,
+    Emitter<PostState> emit,
+  ) async {
+    if (state.hasReachedMax) return;
+
+    try {
+      final posts = await _fetchPosts(startIndex: state.posts.length);
+
+      if (posts.isEmpty) {
+        return emit(state.copyWith(hasReachedMax: true));
+      }
+
+      emit(
+        state.copyWith(
+          status: PostStatus.success,
+          posts: [...state.posts, ...posts],
+        ),
+      );
+    } catch (_) {
+      emit(state.copyWith(status: PostStatus.failure));
     }
+  }
 
-    emit(
-      state.copyWith(
-        status: PostStatus.success,
-        posts: [...state.posts, ...posts],
+  Future<List<Post>> _fetchPosts({required int startIndex}) async {
+    final response = await httpClient.get(
+      Uri.https(
+        'jsonplaceholder.typicode.com',
+        '/posts',
+        <String, String>{'_start': '$startIndex', '_limit': '$_postLimit'},
       ),
     );
-  } catch (_) {
-    emit(state.copyWith(status: PostStatus.failure));
+    if (response.statusCode == 200) {
+      final body = json.decode(response.body) as List;
+      return body.map((dynamic json) {
+        final map = json as Map<String, dynamic>;
+        return Post(
+          id: map['id'] as int,
+          title: map['title'] as String,
+          body: map['body'] as String,
+        );
+      }).toList();
+    }
+    throw Exception('error fetching posts');
   }
-}
-  final http.Client httpClient;
 }
 // our PostBloc will be taking PostEvents as input and outputting PostStates.
 

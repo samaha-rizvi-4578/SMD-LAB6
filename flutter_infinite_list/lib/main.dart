fe49b086a97088fb0d6a53a3e1cac04bf166057c
SamahaRizvi <k214578@nu.edu.pk>
2025-03-20 16:59:57 +0500

    adding imports

--- flutter_infinite_list/lib/main.dart
+++ flutter_infinite_list/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:bloc/bloc.dart';
-import 'package:bloc/bloc.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_infinite_list/app.dart';
@@ -9,13 +8,3 @@ void main() {
   Bloc.observer = const SimpleBlocObserver();
   runApp(const App());
 }
-
-
-
-/* One added bonus of using the bloc library is that we can have access to all Transitions in one place.
-
-The change from one state to another is called a Transition.
-
-Note
-
-A Transition consists of the current state, the event, and the next state.*/ */
